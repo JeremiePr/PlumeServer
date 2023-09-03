75d36d1e96ac26c2800ff4c547eed15b55aff6da
Jerem <jeremie.primas@gmail.com>
2023-09-03 20:56:11 +0200

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 import { PlumeServer } from './main/core';
 import { controller, fromBody, fromHeader, fromQuery, fromRoute, httpDelete, httpGet, httpPatch, httpPost, httpPut, injectable } from './main/decorators';
 
-const classDecorators = { injectable, controller };
-const endpointDecorators = { httpGet, httpPost, httpPut, httpPatch, httpDelete };
-const endpointArgumentDecorators = { fromQuery, fromRoute, fromBody, fromHeader };
-
-export default { PlumeServer, ...classDecorators, ...endpointDecorators, ...endpointArgumentDecorators };
+export { PlumeServer, injectable, controller, httpGet, httpPost, httpPut, httpPatch, httpDelete, fromQuery, fromRoute, fromBody, fromHeader };
