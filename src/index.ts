1c63b86db7ede0c597b1c90968e73c04cb62cc45
Jerem <jeremie.primas@gmail.com>
2023-09-04 23:05:06 +0200

    Add inject export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { PlumeServer } from './main/core';
-import { Controller, FromBody, FromHeader, FromQuery, FromRoute, HttpDelete, HttpGet, HttpPatch, HttpPost, HttpPut, Injectable } from './main/decorators';
+import { Controller, FromBody, FromHeader, FromQuery, FromRoute, HttpDelete, HttpGet, HttpPatch, HttpPost, HttpPut, Injectable, Inject } from './main/decorators';
 
-export { PlumeServer, Injectable, Controller, HttpGet, HttpPost, HttpPut, HttpPatch, HttpDelete, FromQuery, FromRoute, FromBody, FromHeader };
+export { PlumeServer, Inject, Injectable, Controller, HttpGet, HttpPost, HttpPut, HttpPatch, HttpDelete, FromQuery, FromRoute, FromBody, FromHeader };
