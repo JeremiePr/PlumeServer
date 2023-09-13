0597a6c183ec5f27442e8803add102d3c202e81b
Jerem <jeremie.primas@gmail.com>
2023-09-14 01:06:04 +0200

    Add export index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { PlumeServer } from './main/core';
-import { Controller, FromBody, FromHeader, FromQuery, FromRoute, HttpDelete, HttpGet, HttpPatch, HttpPost, HttpPut, Injectable, Inject } from './main/decorators';
+import { Controller, FromBody, FromHeader, FromQuery, FromRoute, HttpDelete, HttpGet, HttpPatch, HttpPost, HttpPut, Injectable, Inject, RequestHandler } from './main/decorators';
 
-export { PlumeServer, Inject, Injectable, Controller, HttpGet, HttpPost, HttpPut, HttpPatch, HttpDelete, FromQuery, FromRoute, FromBody, FromHeader };
+export { PlumeServer, Inject, Injectable, Controller, HttpGet, HttpPost, HttpPut, HttpPatch, HttpDelete, FromQuery, FromRoute, FromBody, FromHeader, RequestHandler };
